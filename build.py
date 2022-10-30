94c1653f46a05278e6e0d8c8d5f8b8375da53b20
Alex Zaslavskis <sahsariga111@gmail.com>
2022-10-30 20:26:00 +0000

    add icon option

--- build.py
+++ build.py
@@ -40,19 +40,14 @@ def main():
     os.chdir('./coda-io-desktop')
     run('npm install ')
     run('npm install electron-packager --global ')
-    #run('npm start &')
-    run(f'npx electron-packager . coda-electron-app --platform={get_os()}')
+    run('npm start &')
+    run(f'npx electron-packager . coda-electron-app --platform={get_os()} --icon {os.path.abspath("icon.ico")}')
     shutil.make_archive(f'coda_app_{get_os()}', 'zip', get_dir_with_app())
     perpare_installer()
+    run(f'"C:\Program Files (x86)\Inno Setup 6\Compil32.exe" {os.path.abspath("installer_build.iss")}')
     
 
 pass
 
-def debug():
-    run('git clone https://github.com/alex5250/coda-io-desktop.git')
-    os.chdir('./coda-io-desktop')
-    perpare_installer()
-pass
 
-#main()
-debug()
+main()
